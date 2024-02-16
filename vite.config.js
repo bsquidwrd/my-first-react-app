390a77f44112d1142c8f2c9dfad9dfb73746413d
bsquidwrd <git@bsquid.io>
2024-02-16 23:47:12 +0000

    Update to add base to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/my-first-react-app/",
   server: {
     port: 3000,
   }
