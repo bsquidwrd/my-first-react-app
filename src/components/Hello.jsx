e1085cd6ae44ef8d29259ca8ef16047d285441bb
bsquidwrd <git@bsquid.io>
2024-02-21 05:14:04 +0000

    React For Beginners 12

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,8 +1,9 @@
-function Hello({ name, message, emoji }) {
+function Hello({ person }) {
   return (
     <div>
       <h1>
-        "{message}" - {name} {emoji}
+        "{person.message}" - {person.name} {person.emoji}
+        {person.seatNumbers}
       </h1>
     </div>
   );
