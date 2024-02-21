f3b486f1582ab4a2fe0d12b3bdc0d718f4e5c026
bsquidwrd <git@bsquid.io>
2024-02-21 04:24:31 +0000

    React For Beginners 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-function App() {
+import Hello from "./components/Hello";
 
+function App() {
   return (
     <div className="App">
-      Hello There!
+      <Hello />
     </div>
   )
 }
---
+++ src/components/Hello.jsx
@@ -0,0 +1,9 @@
+function Hello() {
+    return (
+        <h1>
+            Hello from a component!
+        </h1>
+    )
+}
+
+export default Hello
