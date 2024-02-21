22e537745d836f0d34c6d6a5765877af7e22d23b
bsquidwrd <git@bsquid.io>
2024-02-21 04:56:30 +0000

    React For Beginners 9

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import Hello from "./components/Hello";
 function App() {
   return (
     <div className="App">
-      <Hello />
+      <Hello name="Hank" message="I sell propane and propane accessories. " />
+      <Hello name="Dale" message="Pocket Sand Sh-sh-sha" />
+      <Hello name="Boomhauer" message="Dang ol' love, man." />
+      <Hello
+        name="Bill"
+        message="At least if you're feeling full, you're feeling something."
+      />
     </div>
   );
 }
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,7 +1,9 @@
-function Hello() {
+function Hello(props) {
   return (
     <div>
-      <h1>Hello from a component!</h1> <h1>Wow!</h1>
+      <h1>
+        "{props.message}" - {props.name}
+      </h1>
     </div>
   );
 }
