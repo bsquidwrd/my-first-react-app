0fb125b392a93978f6ebef2fe187a335a27ab80d
bsquidwrd <git@bsquid.io>
2024-02-23 04:01:25 +0000

    React For Beginners 15

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,3 +1,5 @@
+import Fruit from "./Fruit";
+
 export default function Fruits() {
   //const fruits = ["Apple", "Mango", "Banana", "Orange", "Pineapple"];
   const fruits = [
@@ -11,9 +13,12 @@ export default function Fruits() {
     <div>
       <ul>
         {fruits.map((fruit) => (
-          <li key={fruit.name}>
-            {fruit.emoji} {fruit.name} - ${fruit.price}
-          </li>
+          <Fruit
+            key={fruit.name}
+            name={fruit.name}
+            price={fruit.price}
+            emoji={fruit.emoji}
+          />
         ))}
       </ul>
     </div>
