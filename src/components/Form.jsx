bb7444bb18b444c3c0ea4f74402c66cc9feafe6f
bsquidwrd <git@bsquid.io>
2024-02-23 05:37:03 +0000

    React For Beginners 26

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
 
 export default function Form() {
-  const [firstName, setFirstName] = useState("");
+  const [name, setName] = useState({ firstName: "John", lastName: "Smith" });
 
   return (
     <div>
+      <h1>
+        Hello {name.firstName} {name.lastName}
+      </h1>
       <form>
         <input
-          onChange={(event) => setFirstName(event.target.value)}
+          onChange={(event) =>
+            setName({ ...name, firstName: event.target.value })
+          }
           type="text"
-          value={firstName}
+          value={name.firstName}
+        />
+        <input
+          onChange={(event) =>
+            setName({ ...name, lastName: event.target.value })
+          }
+          type="text"
+          value={name.lastName}
         />
       </form>
     </div>
