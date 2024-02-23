8f6fc6ea8c55b076bed50bd34e888c4d5541fe75
bsquidwrd <git@bsquid.io>
2024-02-23 03:45:22 +0000

    React For Beginners 13

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,9 @@
-import Hello from "./components/Hello";
+import Fruits from "./components/Fruits";
 
 function App() {
-  const hankPerson = {
-    name: "Hank",
-    message: "I sell propane and propane accessories",
-    emoji: "🤠",
-    seatNumbers: [1, 4, 7],
-  };
-
-  const dalePerson = {
-    name: "Dale",
-    message: "Pocket Sand Sh-sh-sha",
-    emoji: "🪳",
-    seatNumbers: [2, 5, 8],
-  };
-
-  const boomhauerPerson = {
-    name: "Boomhauer",
-    message: "Dang ol' love, man",
-    emoji: "🏄‍♂️",
-    seatNumbers: [3, 6, 9],
-  };
-
-  const billPerson = {
-    name: "Bill",
-    message: "At least if you're feeling full, you're feeling something",
-    emoji: "🪖",
-    seatNumbers: [4, 7, 0],
-  };
-
   return (
     <div className="App">
-      <Hello person={hankPerson} />
-      <Hello person={dalePerson} />
-      <Hello person={boomhauerPerson} />
-      <Hello person={billPerson} />
+      <Fruits />
     </div>
   );
 }
