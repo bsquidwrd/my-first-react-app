be34f78eac2c6f168efa52e418818bf9e36fa7f0
bsquidwrd <git@bsquid.io>
2024-02-23 04:21:26 +0000

    React For Beginners 18

--- src/components/ConditionalComponent.jsx
+++ src/components/ConditionalComponent.jsx
@@ -2,13 +2,6 @@ import Code from "./Code";
 import Welcome from "./Welcome";
 
 export default function ConditionalComponent() {
-  let message;
   const display = false;
-
-  if (display) {
-    message = <h1>This is message 1</h1>;
-  } else {
-    message = <h1>This is message 2</h1>;
-  }
-  return message;
+  return display ? <Welcome /> : <Code />;
 }
